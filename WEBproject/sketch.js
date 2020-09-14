c4d334dc5367b14c93e7bb557e8f4b54d3505b75
barbfromhr <70112119+barbfromhr@users.noreply.github.com>
2020-09-13 21:54:18 -0400

    first iteration of the site
    simple mousedrag and ellipse

---
+++ WEBproject/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(1024, 768);
+  //Liking the idea of having a different    background load up for each veiwer 
+  background(random(100),random(100),random(100));
+}
+
+function draw() {
+      ellipse(width/2,height/2,50,50);
+      ellipse(mouseX, mouseY, 20, 20);
+}
+function mousePressed() {
+  clear();
+  background(128);
+}
+
+
+  
+
+
+
+  
